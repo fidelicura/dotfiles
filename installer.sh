27471d2549a8af505b8d7735a73e50fbefea557b
fidelicura <fidelicura@gmail.com>
2023-05-14 13:49:51 +0300

    deleted ranger and joplin installation

--- installer.sh
+++ installer.sh
@@ -26,10 +26,9 @@ $INST -Syu
 printf "\n\n[$] > Installing libraries...\n\n"
 $INST -S unzip unrar xclip xorg xorg-xinit xorg-xdm fuse2 tree nvidia base-devel
 printf "\n\n[$] > Installing utilities...\n\n"
-$INST -S xcolor feh fzf flameshot papirus-icon-theme ranger wget curl
+$INST -S xcolor feh fzf flameshot papirus-icon-theme wget curl
 printf "\n\n[$] > Installing applications...\n\n"
 $INST -S qtile picom rofi alacritty neovim zathura zathura-pdf-poppler firefox telegram-desktop discord qbittorrent 
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 printf "\n\n[$] > Installing programming languages...\n\n"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile default -y
 $INST -S python gcc
