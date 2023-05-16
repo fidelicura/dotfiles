3aa72b004de422433e5c185868fad8a22e9d6d3a
fidelicura <fidelicura@gmail.com>
2023-05-16 13:17:31 +0300

    changed xrate from 100 30 to 175 35

--- src/qtile/autostart.sh
+++ src/qtile/autostart.sh
@@ -3,5 +3,5 @@
 feh --bg-fill $HOME/.config/bg/bg.png &
 setxkbmap -layout "us,ru" -option "grp:switch,grp:alt_shift_toggle" &
 xrandr --output HDMI-1 --mode 1920x1080 --rate 75 &
-xset r rate 100 30 &
+xset r rate 175 35 &
 picom -f
